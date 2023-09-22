ab7ebcc64ef57af078649a6afc15b0d481f764cd
Lerry Augusto <lerry.pirani@gmail.com>
2023-09-22 20:36:23 -0300

    gridview for news in home page with customScrollView

--- lib/src/features/view/home/home_page.dart
+++ lib/src/features/view/home/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import '../../../core/constants/app_data.dart';
 import '../../common_widgets/custom_appbar.dart';
@@ -12,7 +11,110 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final gridNews = AppData.getNews
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      key: scaffoldKey,
+      endDrawer: const CustomDrawer(),
+      appBar: CustomAppbar(
+        titleText: 'Notícias',
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              scaffoldKey.currentState!.openEndDrawer();
+            },
+            icon: const Icon(Icons.menu),
+          ),
+        ],
+      ),
+      body: SizedBox(
+        height: MediaQuery.sizeOf(context).height,
+        child: CustomScrollView(
+          slivers: [
+            SliverPadding(
+              padding: const EdgeInsets.all(8),
+              sliver: SliverToBoxAdapter(
+                child: SizedBox(
+                  child: Column(
+                    children: [
+                      Image.network(
+                        AppData.getNews.first['mainImg'] as String,
+                      ),
+                      Text(
+                        AppData.getNews.first['title'] as String,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        AppData.getNews.first['content'] as String,
+                        style: const TextStyle(fontWeight: FontWeight.normal),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.all(8),
+              sliver: SliverGrid.extent(
+                crossAxisSpacing: 4,
+                maxCrossAxisExtent: (MediaQuery.sizeOf(context).width / 2),
+                children: AppData.getNews
+                    .map((e) => SizedBox(
+                          child: Column(
+                            children: [
+                              Image.network(
+                                e['mainImg'] as String,
+                              ),
+                              Text(
+                                e['title'] as String,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
+                        ))
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/* SliverFillRemaining(
+                child: GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 8,
+                  ),
+                  itemCount: AppData.getNews.length - 1,
+                  itemBuilder: (context, index)  {
+
+
+                    return SizedBox(
+                    child: Column(
+                      children: [
+                        Image.network(
+                          AppData.getNews[index + 1]['mainImg'] as String,
+                        ),
+                        Text(
+                          AppData.getNews[index + 1]['title'] as String,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  );}
+                ),
+              ), */
+
+/* final gridNews = AppData.getNews
         .map(
           (e) => StaggeredGridTile.count(
             crossAxisCellCount: 1,
@@ -33,23 +135,7 @@ class HomePage extends StatelessWidget {
         )
         .toList();
 
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      key: scaffoldKey,
-      endDrawer: const CustomDrawer(),
-      appBar: CustomAppbar(
-        titleText: 'Notícias',
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              scaffoldKey.currentState!.openEndDrawer();
-            },
-            icon: const Icon(Icons.menu),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
+SingleChildScrollView(
         child: StaggeredGrid.count(
           crossAxisCount: 2,
           children: [
@@ -79,7 +165,4 @@ class HomePage extends StatelessWidget {
             ...gridNews,
           ],
         ),
-      ),
-    );
-  }
-}
+      ), */
